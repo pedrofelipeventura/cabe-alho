53369c714f98ddbfb627d8055cc8750e61ad8a32
pedro ventura <168668778+pedrofelipeventura@users.noreply.github.com>
2025-02-15 21:51:07 -0300

    Create utilidades.h

---
+++ utilidades.h
@@ -0,0 +1,27 @@
+// *******************
+// * Aluno:  Sofia Freitas Batista Prata                   *
+// * Matrícula: 20241045050448                             *
+// * Avaliação 04: Trabalho Final                          *
+// * 04.505.23 - 2024.2 - Prof. Daniel Ferreira            *
+// * Compilador: gcc (MinGW.org GCC-6.3.0-1) 6.3.0         *
+// *******************
+// * Aluno: Pedro Felipe Monteiro Ventura                  *
+// * Matrícula: 20241045050294                             *
+// * Avaliação 04: Trabalho Final                          *
+// * 04.505.23 - 2024.2 - Prof. Daniel Ferreira            *
+// * Compilador: gcc (MinGW.org GCC-6.3.0-1) 6.3.0         *
+// *******************
+// * Aluno: Miguel Antonio Florencio Felix                 *
+// * Matrícula: 2024104505                                 *
+// * Avaliação 04: Trabalho Final                          *
+// * 04.505.23 - 2024.2 - Prof. Daniel Ferreira            *
+// * Compilador: gcc (MinGW.org GCC-6.3.0-1) 6.3.0         *
+// *******************
+
+#ifndef UTILIDADES_H
+#define UTILIDADES_H
+
+void criarPastaDestino(const char *pasta);
+void processarPasta(const char *pasta, const char *pasta_destino);
+
+#endif
