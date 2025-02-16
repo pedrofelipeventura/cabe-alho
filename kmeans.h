bd743ca1e76c47753e36c1e8bd0324753f0f2e5b
pedro ventura <168668778+pedrofelipeventura@users.noreply.github.com>
2025-02-15 21:48:48 -0300

    Create kmeans.h

---
+++ kmeans.h
@@ -0,0 +1,27 @@
+// *******************
+// * Aluno:  Sofia Freitas Batista Prata                   *
+// * Matrícula: 20241045050448                             *
+// * Avaliação 04: Trabalho Final                          *
+// * 04.505.23 - 2024.2 - Prof. Daniel Ferreira            *
+// * Compilador: gcc (MinGW.org GCC-6.3.0-1) 6.3.0         *
+// *******************
+// * Aluno: Pedro Felipe Monteiro Ventura                  *
+// * Matrícula: 20241045050294                             *
+// * Avaliação 04: Trabalho Final                          *
+// * 04.505.23 - 2024.2 - Prof. Daniel Ferreira            *
+// * Compilador: gcc (MinGW.org GCC-6.3.0-1) 6.3.0         *
+// *******************
+// * Aluno: Miguel Antonio Florencio Felix                 *
+// * Matrícula: 2024104505                                 *
+// * Avaliação 04: Trabalho Final                          *
+// * 04.505.23 - 2024.2 - Prof. Daniel Ferreira            *
+// * Compilador: gcc (MinGW.org GCC-6.3.0-1) 6.3.0         *
+// *******************
+
+#ifndef KMEANS_H
+#define KMEANS_H
+#include "imagem.h"
+
+void kmeans(PGM *pgm, int k, int *clusters);
+
+#endif
